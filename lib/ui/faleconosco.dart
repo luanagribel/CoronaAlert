1591576cbd775be1a374fd4b918aa84705b028bf
luanagribel <luanagribel@hotmail.com>
2021-05-29 13:13:39 -0300

    Refatorando fale conosco

--- lib/ui/faleconosco.dart
+++ lib/ui/faleconosco.dart
@@ -30,101 +30,225 @@ class FaleConoscoPageState extends State<FaleConoscoPage> {
   }
 
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    double width = size.width > size.height ? size.height : size.width;
+
+    if (width < 722) {
+      return FaleConoscoScreenPhone(context);
+    } else if (width > 722) {
+      return FaleConoscoScreenDesk(context);
+    } else {
+      return FaleConoscoScreenDesk(context);
+    }
+  }
+
+  Scaffold FaleConoscoScreenPhone(BuildContext context) {
+    Color baseColor = Color(0xFFF2F2F2);
+    return Scaffold(
+        body: ListView(
+      children: [
+        Container(
+          width: MediaQuery.of(context).size.width,
+          margin: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 50.0),
+          color: baseColor,
+          child: Image(
+            image: AssetImage("./assets/FaleConoscoMobile.png"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        Container(
+          width: MediaQuery.of(context).size.width,
+          margin: EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 40.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Preencha os campos abaixo para nos enviar dúvidas ou sugestões:',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.lightBlue[900],
+                ),
+              ),
+            ],
+          ),
+        ),
+        FractionallySizedBox(
+          widthFactor: 0.8,
+          child: TextField(
+            controller: nameController,
+            obscureText: false,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Nome Completo',
+              fillColor: Colors.grey[300],
+              filled: true,
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        FractionallySizedBox(
+          widthFactor: 0.8,
+          child: TextField(
+            controller: mailController,
+            obscureText: false,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Email',
+              fillColor: Colors.grey[300],
+              filled: true,
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        FractionallySizedBox(
+          widthFactor: 0.8,
+          child: TextField(
+            controller: msgController,
+            obscureText: false,
+            decoration: InputDecoration(
+              contentPadding: const EdgeInsets.symmetric(vertical: 40.0),
+              border: OutlineInputBorder(),
+              labelText: '  Mensagem',
+              fillColor: Colors.grey[300],
+              filled: true,
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 30,
+        ),
+        FractionallySizedBox(
+          widthFactor: 0.5,
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              primary: Colors.lightBlue[900],
+            ),
+            child: Text('Enviar'),
+            onPressed: _createContato,
+          ),
+        ),
+        SizedBox(
+          height: 30,
+        ),
+      ],
+    ));
+  }
+
+  Scaffold FaleConoscoScreenDesk(BuildContext context) {
     Color baseColor = Color(0xFFF2F2F2);
 
     return Scaffold(
-      body: ListView(
+        body: ListView(children: [
+      Container(
+        width: MediaQuery.of(context).size.width,
+        margin: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 50.0),
+        color: baseColor,
+        child: Image(
+          image: AssetImage("./assets/FaleConoscoDesk.png"),
+          fit: BoxFit.cover,
+        ),
+      ),
+      Container(
+        width: MediaQuery.of(context).size.width,
+        margin: EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 40.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Preencha os campos abaixo para nos enviar dúvidas ou sugestões:',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.lightBlue[900],
+              ),
+            ),
+          ],
+        ),
+      ),
+      Wrap(
+        alignment: WrapAlignment.center,
+        spacing: 8.0,
         children: [
           Container(
-            width: MediaQuery.of(context).size.width,
-            margin: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 50.0),
-            color: baseColor,
-            child: Image(
-              image: AssetImage("./assets/FaleConoscoDesk.png"),
-              fit: BoxFit.cover,
-            ),
+            child: Column(children: [
+              FractionallySizedBox(
+                widthFactor: 0.2,
+                child: TextField(
+                  controller: nameController,
+                  obscureText: false,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Nome Completo',
+                    fillColor: Colors.grey[300],
+                    filled: true,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              FractionallySizedBox(
+                widthFactor: 0.2,
+                child: TextField(
+                  controller: mailController,
+                  obscureText: false,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Email',
+                    fillColor: Colors.grey[300],
+                    filled: true,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+            ]),
           ),
           Container(
-            width: MediaQuery.of(context).size.width,
-            margin: EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 40.0),
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text(
-                  'Preencha os campos abaixo para nos enviar dúvidas ou sugestões:',
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.lightBlue[900],
+                FractionallySizedBox(
+                  widthFactor: 0.3,
+                  child: TextField(
+                    controller: msgController,
+                    obscureText: false,
+                    decoration: InputDecoration(
+                      contentPadding:
+                          const EdgeInsets.symmetric(vertical: 50.0),
+                      border: OutlineInputBorder(),
+                      labelText: '  Mensagem',
+                      fillColor: Colors.grey[300],
+                      filled: true,
+                    ),
                   ),
+                ),
+                SizedBox(
+                  height: 30,
                 ),
               ],
             ),
           ),
-          FractionallySizedBox(
-            widthFactor: 0.8,
-            child: TextField(
-              controller: nameController,
-              obscureText: false,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Nome Completo',
-                fillColor: Colors.grey[300],
-                filled: true,
-              ),
-            ),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          FractionallySizedBox(
-            widthFactor: 0.8,
-            child: TextField(
-              controller: mailController,
-              obscureText: false,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Email',
-                fillColor: Colors.grey[300],
-                filled: true,
-              ),
-            ),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          FractionallySizedBox(
-            widthFactor: 0.8,
-            child: TextField(
-              controller: msgController,
-              obscureText: false,
-              decoration: InputDecoration(
-                contentPadding: const EdgeInsets.symmetric(vertical: 40.0),
-                border: OutlineInputBorder(),
-                labelText: '  Mensagem',
-                fillColor: Colors.grey[300],
-                filled: true,
-              ),
-            ),
-          ),
-          SizedBox(
-            height: 30,
-          ),
-          FractionallySizedBox(
-            widthFactor: 0.5,
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                primary: Colors.lightBlue[900],
-              ),
-              child: Text('Enviar'),
-              onPressed: _createContato,
-            ),
-          ),
-          SizedBox(
-            height: 30,
-          ),
         ],
       ),
-    );
+      FractionallySizedBox(
+        widthFactor: 0.1,
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            primary: Colors.lightBlue[900],
+          ),
+          child: Text('Enviar'),
+          onPressed: _createContato,
+        ),
+      ),
+      SizedBox(
+        height: 30,
+      ),
+    ]));
   }
 }
