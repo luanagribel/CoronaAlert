70025cbd22cb2abdf24a313c5646ced120538566
luanagribel <luanagribel@hotmail.com>
2021-06-02 21:52:48 -0300

    Melhorias layout vacinometro e faq

--- lib/ui/calendario.dart
+++ lib/ui/calendario.dart
@@ -215,32 +215,44 @@ class CalendarioPage extends StatelessWidget {
   }
 
   Scaffold calendScreenDesktop(BuildContext context) {
-    Color baseColor = Colors.white;
+    Color baseColor = Color(
+      0xFF252525,
+    );
 
     return Scaffold(
-      backgroundColor: Colors.grey[200],
+      backgroundColor: Color(0xFF121212),
       body: ListView(
         children: [
+          SizedBox(
+            height: 130,
+          ),
           Container(
-            width: MediaQuery.of(context).size.width,
-            margin: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 10.0),
-            color: baseColor,
-            child: Image(
-              image: AssetImage("./assets/calendDesk.png"),
-              fit: BoxFit.cover,
+            margin: EdgeInsets.only(left: 40),
+            child: Text(
+              "VACINÔMETRO E DÚVIDAS FREQUENTES",
+              textAlign: TextAlign.left,
+              style: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.bold,
+                color: Color(0xFF03A688),
+              ),
             ),
+          ),
+          SizedBox(
+            height: 20,
           ),
           Row(
             children: [
               Align(
                 alignment: Alignment.centerLeft,
                 child: Container(
-                  width: MediaQuery.of(context).size.width / 2,
+                  width: MediaQuery.of(context).size.width / 3,
+                  // height: MediaQuery.of(context).size.width / 2,
                   margin: EdgeInsets.fromLTRB(30.0, 0.0, 0.0, 0.0),
                   padding: EdgeInsets.all(10),
                   decoration: BoxDecoration(
-                    color: Colors.white,
-                    border: Border.all(width: 1.0, color: Colors.white),
+                    color: baseColor,
+                    border: Border.all(width: 4.0, color: baseColor),
                     borderRadius: BorderRadius.circular(20),
                   ),
                   child: Column(children: [
@@ -256,10 +268,10 @@ class CalendarioPage extends StatelessWidget {
               Container(
                 width: MediaQuery.of(context).size.width / 2.5,
                 margin: EdgeInsets.fromLTRB(40.0, 0.0, 0.0, 0.0),
-                padding: EdgeInsets.all(10),
+                padding: EdgeInsets.all(20),
                 decoration: BoxDecoration(
-                  color: Colors.white,
-                  border: Border.all(width: 1.0, color: Colors.white),
+                  color: baseColor,
+                  border: Border.all(width: 1.0, color: baseColor),
                   borderRadius: BorderRadius.circular(20),
                 ),
                 child: Column(children: [
@@ -267,37 +279,39 @@ class CalendarioPage extends StatelessWidget {
                     height: 30,
                   ),
                   Align(
-                    alignment: Alignment.centerLeft,
+                    alignment: Alignment.center,
                     child: Icon(
                       Icons.contact_support,
-                      color: Color(0x17202A).withOpacity(0.8),
-                      size: 36,
+                      color: Color(0xFF03A688),
+                      size: 28,
                     ),
                   ),
                   SizedBox(
                     height: 16,
                   ),
                   Align(
-                    alignment: Alignment.centerLeft,
+                    alignment: Alignment.center,
                     child: Text(
-                      "Foi o município quem determinou a ordem dos grupos a serem vacinados?",
+                      "FOI O MUNICÍPIO QUEM DETERMINOU A ORDEM DOS GRUPOS A SEREM VACINADOS?",
+                      textAlign: TextAlign.center,
                       style: TextStyle(
-                        fontSize: 30,
+                        fontSize: 14,
                         fontWeight: FontWeight.bold,
-                        color: Color(0x17202A).withOpacity(0.8),
+                        color: Color(0xFF03A688),
                       ),
                     ),
                   ),
                   SizedBox(
-                    height: 10,
+                    height: 15,
                   ),
                   Align(
-                    alignment: Alignment.centerLeft,
+                    //alignment: Alignment.centerLeft,
                     child: Text(
                       "Não, os grupos prioritários e a ordem em que eles serão vacinados foram definidos pelo Ministério da Saúde, e pela Secretaria do Estado de Saúde de Minas Gerais, por meio da Deliberação CIB-SUS/MG 3314, de 29 de janeiro de 2021.",
+                      textAlign: TextAlign.justify,
                       style: TextStyle(
-                        fontSize: 18,
-                        color: Colors.black,
+                        fontSize: 14,
+                        color: Colors.grey[100],
                       ),
                     ),
                   ),
@@ -305,37 +319,38 @@ class CalendarioPage extends StatelessWidget {
                     height: 40,
                   ),
                   Align(
-                    alignment: Alignment.centerLeft,
+                    alignment: Alignment.center,
                     child: Icon(
                       Icons.contact_support,
-                      color: Color(0x17202A).withOpacity(0.8),
-                      size: 36,
+                      color: Color(0xFF03A688),
+                      size: 28,
                     ),
                   ),
                   SizedBox(
                     height: 16,
                   ),
                   Align(
-                    alignment: Alignment.centerLeft,
+                    alignment: Alignment.center,
                     child: Text(
-                      "Qual o grupo prioritário em que estamos hoje no município?",
+                      "QUAL O GRUPO PRIORITÁRIO EM QUE ESTAMOS HOJE NO MUNICÍPIO?",
                       style: TextStyle(
-                        fontSize: 30,
+                        fontSize: 14,
                         fontWeight: FontWeight.bold,
-                        color: Color(0x17202A).withOpacity(0.8),
+                        color: Color(0xFF03A688),
                       ),
                     ),
                   ),
                   SizedBox(
-                    height: 10,
+                    height: 15,
                   ),
                   Align(
                     alignment: Alignment.centerLeft,
                     child: Text(
                       "Grupo de comorbidades: Pessoas com Síndrome de Down (todos acima de 18 anos)\nRenal crônico: Que faz hemodiálise e diálise (todos acima de 18 anos)\nDemais Comorbidades: de 59 a 55 anos.\n(Grupo em anexo. Pessoas com deficiência cadastradas no BPC: de 59 a 55 anos.)",
+                      textAlign: TextAlign.justify,
                       style: TextStyle(
-                        fontSize: 18,
-                        color: Colors.black,
+                        fontSize: 14,
+                        color: Colors.grey[100],
                       ),
                     ),
                   ),
@@ -343,13 +358,14 @@ class CalendarioPage extends StatelessWidget {
                     height: 20,
                   ),
                   Align(
-                    alignment: Alignment.centerLeft,
+                    // alignment: Alignment.centerLeft,
                     child: Text(
                       "Nota: Gestantes e Puérperas suspensão a vacinação com a vacina do Laboratório Fio Cruz/ Astrazenica, de acordo com o ofício em anexo.O Município recebeu apenas o laboratório Fiocruz/Astrazenica, por esse motivo a suspensão da vacina para Gestantes e Puérperas.",
+                      textAlign: TextAlign.justify,
                       style: TextStyle(
-                        fontSize: 18,
+                        fontSize: 14,
                         fontWeight: FontWeight.bold,
-                        color: Colors.black,
+                        color: Colors.grey[100],
                       ),
                     ),
                   ),
@@ -357,37 +373,38 @@ class CalendarioPage extends StatelessWidget {
                     height: 40,
                   ),
                   Align(
-                    alignment: Alignment.centerLeft,
+                    alignment: Alignment.center,
                     child: Icon(
                       Icons.contact_support,
-                      color: Color(0x17202A).withOpacity(0.8),
-                      size: 36,
+                      color: Color(0xFF03A688),
+                      size: 28,
                     ),
                   ),
                   SizedBox(
                     height: 16,
                   ),
                   Align(
-                    alignment: Alignment.centerLeft,
+                    alignment: Alignment.center,
                     child: Text(
-                      "Como posso realizar o agendamento para os idosos acima de 60 – 64?",
+                      "COMO POSSO REALIZAR O AGENDAMENTO PARA OS IDOSOS ACIMA DE 60 - 64?",
                       style: TextStyle(
-                        fontSize: 30,
+                        fontSize: 14,
                         fontWeight: FontWeight.bold,
-                        color: Color(0x17202A).withOpacity(0.8),
+                        color: Color(0xFF03A688),
                       ),
                     ),
                   ),
                   SizedBox(
-                    height: 10,
+                    height: 15,
                   ),
                   Align(
-                    alignment: Alignment.centerLeft,
+                    //alignment: Alignment.centerLeft,
                     child: Text(
                       "Caso algum idoso nesta faixa de idade ainda não tenha recebido a vacina, solicitamos que a família, ou amigos procurem o Posto de Saúde em que o idoso for registrado, ou o posto mais próximo, de acordo com seu bairro para saber quais os horários disponíveis e se a vacina será feita em domicílio, ou não.",
+                      textAlign: TextAlign.justify,
                       style: TextStyle(
-                        fontSize: 18,
-                        color: Colors.black,
+                        fontSize: 14,
+                        color: Colors.grey[100],
                       ),
                     ),
                   ),
@@ -398,8 +415,8 @@ class CalendarioPage extends StatelessWidget {
                     "Dados fornecidos pela Secretaria de Saúde - Santa Rita do Sapucaí\nInformações (3471-5609 - 3471-2006)",
                     textAlign: TextAlign.center,
                     style: TextStyle(
-                      fontSize: 16,
-                      color: Colors.black,
+                      fontSize: 12,
+                      color: Colors.grey[100],
                       fontWeight: FontWeight.bold,
                     ),
                   ),
